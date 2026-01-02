103fb375bf26c03c41acb384b6e542fb0c3ba1a1
Gayanand <gayanand.patel@gmail.com>
2026-01-02 13:17:37 +0530

    add complete demo program of Collection Framework

---
+++ 22. Collection Framework/TreeSetDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TreeSetDemo {
+
+    public static void main(String[] args) {
+        
+        TreeSet<Integer> ts=new TreeSet<>(List.of(10,30,50,70,10,40));
+        
+        ts.add(25);
+        
+        //ts.ceiling(55);
+        
+        System.out.println(ts);
+        
+    }
+    
+}
